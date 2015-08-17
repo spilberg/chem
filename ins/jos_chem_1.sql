be0219673c0443d59763b27dd86ce5cab9649b35
Nick Korbut <nick.korbut@gmail.com>
2015-08-17 07:52:40 +0300

    new sql file

---
+++ ins/jos_chem_1.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Авг 17 2015 г., 07:49
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `chem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `jos_chem`
+--
+
+CREATE TABLE IF NOT EXISTS `jos_chem` (
+  `cat_namber` char(20) DEFAULT NULL,
+  `id` int(11) DEFAULT NULL,
+  `mol_weigh` double DEFAULT NULL,
+  `mass` double DEFAULT NULL,
+  `cas_number` varchar(50) DEFAULT NULL,
+  `mdl_number` varchar(50) DEFAULT NULL,
+  `mdl_form` text NOT NULL,
+  `purity` int(11) DEFAULT NULL,
+  `smiles` varchar(100) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `molecular_formula` varchar(50) DEFAULT NULL,
+  `iupac_name` varchar(200) DEFAULT NULL,
+  `price1mg` double DEFAULT NULL,
+  `price2mg` double DEFAULT NULL,
+  `price3mg` double DEFAULT NULL,
+  `price4mg` double DEFAULT NULL,
+  `price5mg` double DEFAULT NULL,
+  `price10mg` double DEFAULT NULL,
+  `price15mg` double DEFAULT NULL,
+  `price20mg` double DEFAULT NULL,
+  `price25mg` double DEFAULT NULL,
+  `price5mmol` double DEFAULT NULL,
+  `price10mmol` double DEFAULT NULL,
+  `price20mmol` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=420;
+
+--
+-- Дамп данных таблицы `jos_chem`
+--
+
+INSERT INTO `jos_chem` (`cat_namber`, `id`, `mol_weigh`, `mass`, `cas_number`, `mdl_number`, `mdl_form`, `purity`, `smiles`, `status`, `molecular_formula`, `iupac_name`, `price1mg`, `price2mg`, `price3mg`, `price4mg`, `price5mg`, `price10mg`, `price15mg`, `price20mg`, `price25mg`, `price5mmol`, `price10mmol`, `price20mmol`) VALUES
+('0110420239', 3387, 372.81134, 35, '13333-86-3', 'MFCD00179413', '  Mrv1542008131511362D          \r\n\r\n\r\n  9  9  0  0  0  0            999 V2000\r\n   -0.6006    1.1220    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\r\n   -0.6006    0.2970    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\r\n   -1.3151   -0.1155    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\r\n   -2.0295    0.2970    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\r\n   -2.0295    1.1220    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\r\n   -1.3151    1.5345    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\r\n   -1.3151    2.3595    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0\r\n   -2.7440   -0.1155    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\r\n   -2.0295    2.7720    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\r\n  1  2  1  0  0  0  0\r\n  1  6  2  0  0  0  0\r\n  2  3  2  0  0  0  0\r\n  3  4  1  0  0  0  0\r\n  4  5  2  0  0  0  0\r\n  5  6  1  0  0  0  0\r\n  6  7  1  0  0  0  0\r\n  4  8  1  0  0  0  0\r\n  7  9  1  0  0  0  0\r\nM  END\r\n', 96, 'OC(=O)C(Cc1c[nH]c2ccccc12)NC(=O)COc3ccc(Cl)cc3', 'in stock', 'C19H17ClN2O4', '2-[2-(4-chlorophenoxy)acetamido]-3-(1H-indol-3-yl)propanoic acid', 23, 16, 57, 23, 22, 16, 57, 16, 17, 22, 17, 53),
+('7010210057', 7098, 342.21634, 42, '13333-86-4', 'MFCD00179414', '', 99, 'Cc1ccc(NC(=O)CC2SC(=N)NC2=O)c(Br)c1', 'virtual', 'C12H12BrN3O2S', 'N-(2-bromo-4-methylphenyl)-2-(2-imino-4-oxo-1,3-thiazolidin-5-yl)acetamide', 23, 18, 55, 23, 12, 18, 55, 18, 22, 12, 22, 52),
+('0127500003', 9818, 238.29448, 440, '13333-86-5', 'MFCD00179415', '', 96, 'Cc1cc(N)n(n1)c2cc(C)c3ccccc3n2', 'virtual', 'C14H14N4', '3-methyl-1-(4-methylquinolin-2-yl)-1H-pyrazol-5-amine', 19, 25, 46, 19, 13, 25, 46, 25, 18, 13, 18, 54),
+('0103420002', 18667, 303.79151, 1585, '13333-86-6', 'MFCD00179416', '', 98, 'CCc1ccc(OCC(=O)Nc2cccc(Cl)c2C)cc1', 'in stock', 'C17H18ClNO2', 'N-(3-chloro-2-methylphenyl)-2-(4-ethylphenoxy)acetamide', 15, 25, 56, 15, 12, 25, 56, 25, 21, 12, 21, 50),
+('0103420025', 18689, 391.48995, 55, '13333-86-7', 'MFCD00179417', '', 92, 'CCS(=O)(=O)c1ccc(O)c(NC(=O)COc2cc(C)ccc2C(C)C)c1', 'in stock', 'C20H25NO5S', 'N-[5-(ethanesulfonyl)-2-hydroxyphenyl]-2-[5-methyl-2-(propan-2-yl)phenoxy]acetamide', 19, 15, 49, 19, 23, 15, 49, 15, 18, 23, 18, 53),
+('0123750075', 22091, 309.43346, 10, '13333-86-8', 'MFCD00179418', '', 92, 'CCN(CC)CC(=O)Nc1sc2CCCCc2c1C(=O)N', 'virtual', 'C15H23N3O2S', '2-[2-(diethylamino)acetamido]-4,5,6,7-tetrahydro-1-benzothiophene-3-carboxamide', 23, 21, 53, 23, 15, 21, 53, 21, 25, 15, 25, 51);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
